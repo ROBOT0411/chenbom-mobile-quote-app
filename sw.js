1fc6c1a4ec9e37839ce8f99ae96252952d88c74e
vectechcb-hue <vectech.cb@gmail.com>
2026-09-23 13:06:32 +0800

    改善手機離線快取

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const CACHE="chenbom-quote-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>r))));
+const CACHE="chenbom-quote-v2";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./sw.js"]))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>r))));
